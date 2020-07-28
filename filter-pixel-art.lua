8410d8e80db33748c6498e3f45b8bc2f833ecf2b
bfxdev <11587816+bfxdev@users.noreply.github.com>
2020-07-28 07:48:26 +0200

    Re-orgnized and cleaned code, only Luminance effect implemented, still experimenting with downscale options

--- filter-pixel-art.lua
+++ filter-pixel-art.lua
@@ -17,24 +17,17 @@
  -- FFI library see https://luajit.org/ext_ffi.html
 -- ffi = require("ffi")
 
------------------------------------------------- SCRIPT GLOBAL FUNCTIONS -----------------------------------------------
+------------------------------------------------- SCRIPT GLOBAL FUNCTIONS ----------------------------------------------
+
+-- Global variable to be used between script global functions and source_info
+-- This is the default preset to apply if a source is created and added to a video source
+default_preset = 1
 
 -- Initial call sequence: script_defaults, script_description, script_load, script_update, script_properties
 -- See https://obsproject.com/docs/scripting.html#script-function-exports
 
--- Called to set default settings (if any) associated with the script.
--- You would typically call Default Value Functions on the settings in order to set its default values.
--- The parameter settings is of type obs_data_t, see https://obsproject.com/docs/reference-settings.html
-function script_defaults(settings)
-
-    print("In script_defaults")
-
-    obslua.obs_data_set_default_int(settings, "default_preset", 1)
-
-end
-
 -- Called to retrieve the description string and display it in the script management dialog
--- NOTE: not officially documented
+-- NOTE: not documented
 function script_description()
 
     print("In script_description")
@@ -50,13 +43,25 @@ function script_description()
 
 end
 
+-- Called to set default settings (if any) associated with the script.
+-- You would typically call Default Value Functions on the settings in order to set its default values.
+-- A default value will create the related setting if not already existing, then passed to the first script_load
+-- The parameter settings is of type obs_data_t, see https://obsproject.com/docs/reference-settings.html
+function script_defaults(settings)
+
+    print("In script_defaults")
+
+    -- Set default preset at first addition of the script to OBS (if changed later on, then the new value is kept)
+    obslua.obs_data_set_default_int(settings, "default_preset", default_preset)
+
+end
+
 -- Called on script startup with specific settings associated with the script.
--- The settings parameter provided is not typically used for settings that are set by the user,
--- instead the parameter is used for any extra internal settings data that may be used in the script.
 function script_load(settings)
 
     print("In script_load")
 
+    -- The source_info is defined below
     print("Calls obs_register_source")
     obslua.obs_register_source(source_info)
 
@@ -66,6 +71,10 @@ end
 function script_update(settings)
 
     print("In script_update")
+
+    default_preset = obslua.obs_data_get_int(settings, "default_preset")
+
+    print("Current default_preset: " .. tostring(obslua.obs_data_get_int(settings, "default_preset")))
 
 end
 
@@ -90,225 +99,17 @@ end
 -- Called when the script is being saved. This is not necessary for settings that are set by the user,
 -- instead this is used for any extra internal settings data that may be used in the script.
 function script_save(settings)
-
     print("In script_save")
-
 end
 
 -- Called when the script is being unloaded.
 function script_unload()
-
     print("In script_unload\n")
-
 end
 
-function set_render_size(data)
-    target = obslua.obs_filter_get_target(data.context)
+------------------------------------------------------ EFFECT FILES ----------------------------------------------------
 
-    local width, height
-    if target == nil then
-        width = 0
-        height = 0
-    else
-        width = obslua.obs_source_get_base_width(target)
-        height = obslua.obs_source_get_base_height(target)
-    end
-
-    data.width = width
-    data.height = height
-
-    --local vs = obslua.gs_effect_get_param_by_name(data.effect, 'ViewSize')
-    --local v = obslua.vec2()
-    --v.x = width
-    --v.y = height
-    --obslua.gs_effect_set_vec2(vs, v)
-
-end
-
------------------------------------------------------- SOURCE INFO -----------------------------------------------------
-
--- Defines members of obs_source_info structure
--- See https://obsproject.com/docs/reference-sources.html
-source_info = {}
-
--- Unique string identifier for the source (required)
-source_info.id = 'filter-pixel-art'
-
--- Type of source (one of INPUT/FILTER/TRANSITION)
-source_info.type = obslua.OBS_SOURCE_TYPE_FILTER
-
--- Source output capability flags (required)
--- A bitwise OR combination of VIDEO/AUDIO/ASYNC/etc
-source_info.output_flags = obslua.OBS_SOURCE_VIDEO
-
--- Get the translated name of the source type
--- NOTE: normally there is an argument "type_data", unclear what it means
-source_info.get_name = function()
-    return "Pixel Art"
-end
-
--- Creates the implementation data for the source
--- Parameters:	settings – Settings to initialize the source with
---              source – Source that this data is associated with
--- Returns:	The implementation data associated with this source (arbitrary data structure)
-source_info.create = function(settings, source)
-
-    print("In source_info.create")
-
-    data = {}
-    data.params = {}
-    data.context = source
-
-    print("Given source: " .. obslua.obs_source_get_name(source))
-
-    obslua.obs_enter_graphics()
-
-    set_render_size(data)
-
-    local error_string = ""
-    data.effect = obslua.gs_effect_create_from_file(script_path() .. 'filter-pixel-art.hlsl', nil)
-    if data.effect == nil then
-        print("Effect file not compiled - Reverting to luminance effect")
-        data.effect = obslua.gs_effect_create(EFFECT_LUMINANCE, "luminance", nil)
-    end
-
-    -- if data.effect ~= nil then
-      --  data.params.hue_steps = obslua.gs_effect_get_param_by_name(data.effect, 'hue_steps')
-      --  data.params.value_steps = obslua.gs_effect_get_param_by_name(data.effect, 'value_steps')
-    -- end
-
-    obslua.obs_leave_graphics()
-
-    if data.effect == nil then
-        print("ERROR: HLSL code not compiled")
-        source_info.destroy(data)
-        return nil
-    end
-
-    source_info.update(data, settings)
-    return data
-end
-
--- Destroys the implementation data for the source
-source_info.destroy = function(data)
-
-    print("In source_info.destroy")
-
-    if data.effect ~= nil then
-        obslua.obs_enter_graphics()
-        obslua.gs_effect_destroy(data.effect)
-        obslua.obs_leave_graphics()
-    end
-end
-
--- Returns the width of the source, this callback is required if this is a video source and is synchronous
-source_info.get_width = function(data)
-    return data.width
-end
-
--- Returns the height of the source, this callback is required if this is a video source and is synchronous
-source_info.get_height = function(data)
-    return data.height
-end
-
--- Sets the default settings for this source
-source_info.get_defaults = function(settings)
-
-    print("In source_info.get_defaults")
-
-    -- obslua.obs_data_set_default_int(settings, SETTING_HUE_STEPS, 5)
-    -- obslua.obs_data_set_default_int(settings, SETTING_VALUE_STEPS, 5)
-end
-
--- Gets the property information of this source (Optional)
-source_info.get_properties = function(data)
-
-    print("In source_info.get_properties")
-
-    props = obslua.obs_properties_create()
-
-    -- obslua.obs_properties_add_int_slider(props, SETTING_HUE_STEPS, TEXT_HUE_STEPS, 1, 10, 1)
-    -- obslua.obs_properties_add_int_slider(props, SETTING_VALUE_STEPS, TEXT_VALUE_STEPS, 1, 10, 1)
-
-    return props
-end
-
--- Updates the settings for this source (Optional)
-source_info.update = function(data, settings)
-
-    print("In source_info.update")
-
-    -- data.hue_steps = obslua.obs_data_get_int(settings, SETTING_HUE_STEPS)
-    -- data.value_steps = obslua.obs_data_get_int(settings, SETTING_VALUE_STEPS)
-
-    set_render_size(data)
-end
-
--- Called when the source has been activated in the main view (visible on stream/recording) (Optional)
-source_info.activate = function(data)
-    print("In source_info.activate")
-end
-
--- Called when the source has been deactivated in the main view (no longer visible on stream/recording) (Optional)
-source_info.deactivate = function(data)
-    print("In source_info.deactivate")
-end
-
--- Called when the source is visible on any display and/or on the main view (Optional) 
-source_info.show = function(data)
-    print("In source_info.show")
-end
-
--- Called when the source is no longer visible on any display and/or on the main view (Optional) 
-source_info.hide = function(data)
-    print("In source_info.hide")
-end
-
--- Called each video frame with the time elapsed (Optional)
-source_info.video_tick = function(data, seconds)
-    set_render_size(data)
-end
-
--- Called when rendering the source with the graphics subsystem
--- If this is an input/transition source, this is called to draw the source texture with the graphics subsystem.
--- If this is a filter source, it wraps source draw calls (for example applying a custom effect with custom parameters
---   to a source). In this case, it’s highly recommended to use the obs_source_process_filter_begin() and
---   obs_source_process_filter_end() functions to automatically handle effect-based filter processing. However, you
---   can implement custom draw handling as desired as well.
--- If the source output capability flags do not include OBS_SOURCE_CUSTOM_DRAW, the source must 
---   use obs_source_draw() to render the source’s texture.
--- NOTE: effect – This parameter is no longer used. Instead, call obs_source_draw()
--- See https://obsproject.com/docs/graphics.html#rendering-video-effect-filters
-source_info.video_render = function(data, effect)
-
-    obslua.obs_source_process_filter_begin(data.context, obslua.GS_RGBA, obslua.OBS_NO_DIRECT_RENDERING)
-
-    -- obslua.gs_effect_set_int(data.params.hue_steps, data.hue_steps)
-    -- obslua.gs_effect_set_int(data.params.value_steps, data.value_steps)
-
-    obslua.obs_source_process_filter_end(data.context, data.effect, data.width, data.height)
-end
-
--- Called when saving custom data for a source.
--- This is a separate function because sometimes a source needs to know when it is being saved so it doesn’t always have
--- to update the current settings until a certain point (Optional)
-source_info.save = function(data, settings)
-    print("In source_info.save")
-end
-
--- Called when loading custom data from saved source data.
--- This is called after all the loading sources have actually been created, allowing the ability to reference other
--- sources if desired (Optional)
-source_info.load = function(data, settings)
-    print("In source_info.load")
-end
-
--- Called when the filter is removed from a source (Optional)
-source_info.filter_remove = function(data, source)
-    print("In source_info.filter_remove")
-end
-
----------------------------------------------- FALLBACK LUMINANCE EFFECT -----------------------------------------------
+-- The code of the effects files is copied here to avoid distributing several files
 
 EFFECT_LUMINANCE = [[
 uniform float4x4 ViewProj; // View-projection matrix set by OBS (required)
@@ -340,3 +141,197 @@ technique Draw
     }
 }
 ]]
+
+------------------------------------------------------ SOURCE INFO -----------------------------------------------------
+
+-- Starts the definition of the global variable of type obs_source_info_t, used to create source object instances
+-- when the filter is added on a video source or re-created in a scene at OBS start.
+-- See https://obsproject.com/docs/reference-sources.html
+source_info = {}
+
+-- Unique string identifier for the source (required)
+source_info.id = 'filter-pixel-art'
+
+-- Type of source (one of INPUT/FILTER/TRANSITION)
+source_info.type = obslua.OBS_SOURCE_TYPE_FILTER
+
+-- Source output capability flags (required)
+-- A bitwise OR combination of VIDEO/AUDIO/ASYNC/etc
+source_info.output_flags = obslua.OBS_SOURCE_VIDEO
+
+-- Get the translated name of the source type
+-- NOTE: normally there is an argument "type_data", unclear what it means
+source_info.get_name = function()
+    return "Pixel Art"
+end
+
+-- Sets the default settings for this source
+source_info.get_defaults = function(settings)
+
+    print("In source_info.get_defaults")
+
+    obslua.obs_data_set_default_int(settings, "width", 320)
+    obslua.obs_data_set_default_int(settings, "height", 200)
+
+end
+
+-- Updates the settings for this source (Optional)
+source_info.update = function(data, settings)
+
+    print("In source_info.update")
+
+    data.width = obslua.obs_data_get_int(settings, "width")
+    data.height = obslua.obs_data_get_int(settings, "height")
+
+end
+
+-- Gets the property information of this source (Optional)
+source_info.get_properties = function(data)
+
+    print("In source_info.get_properties")
+
+    props = obslua.obs_properties_create()
+
+    obslua.obs_properties_add_int(props, "width", "Width", 1, 5000, 1)
+    obslua.obs_properties_add_int(props, "height", "Height", 1, 5000, 1)
+
+    return props
+end
+
+-- Creates the implementation data for the source
+-- Parameters:	settings – Settings to initialize the source with
+--              source – Source that this data is associated with
+-- Returns:	The implementation data associated with this source
+source_info.create = function(settings, source)
+
+    print("In source_info.create")
+
+    -- Inits filter data object (arbitrary data structure used to keep own data between calls)
+    data = {}
+
+    -- The source object instance of the filter itself
+    data.source = source
+
+    -- Compiles shader
+    obslua.obs_enter_graphics()
+    data.effect = obslua.gs_effect_create_from_file(script_path() .. 'filter-pixel-art.hlsl', nil)
+    if data.effect == nil then
+        print("Effect file not compiled - Reverting to luminance effect")
+        data.effect = obslua.gs_effect_create(EFFECT_LUMINANCE, "luminance", nil)
+    end
+    obslua.obs_leave_graphics()
+
+    -- Destroys everything if shader was not compiled properly
+    if data.effect == nil then
+        print("ERROR: HLSL code not compiled")
+        source_info.destroy(data)
+        return nil
+    end
+
+    -- Calls update to initialize the rest of the propertis-managed settings
+    source_info.update(data, settings)
+
+    return data
+end
+
+-- Destroys the implementation data for the source
+source_info.destroy = function(data)
+
+    print("In source_info.destroy")
+
+    if data.effect ~= nil then
+        obslua.obs_enter_graphics()
+        obslua.gs_effect_destroy(data.effect)
+        obslua.obs_leave_graphics()
+    end
+end
+
+-- Returns the width of the source, this callback is required if this is a video source and is synchronous
+source_info.get_width = function(data)
+
+    local target = obslua.obs_filter_get_target(data.source)
+
+    if target ~= nil then
+        --data.width = obslua.obs_source_get_base_width(target)
+        --print("Target: " .. obslua.obs_source_get_name(target))
+    end
+
+    return data.width
+end
+
+-- Returns the height of the source, this callback is required if this is a video source and is synchronous
+source_info.get_height = function(data)
+
+    local target = obslua.obs_filter_get_target(data.source)
+
+    if target ~= nil then
+        --data.height = obslua.obs_source_get_base_height(target)
+    end
+
+    return data.height
+end
+
+-- Called when the source has been activated in the main view (visible on stream/recording) (Optional)
+source_info.activate = function(data)
+    print("In source_info.activate")
+end
+
+-- Called when the source has been deactivated in the main view (no longer visible on stream/recording) (Optional)
+source_info.deactivate = function(data)
+    print("In source_info.deactivate")
+end
+
+-- Called when the source is visible on any display and/or on the main view (Optional) 
+source_info.show = function(data)
+    print("In source_info.show")
+end
+
+-- Called when the source is no longer visible on any display and/or on the main view (Optional) 
+source_info.hide = function(data)
+    print("In source_info.hide")
+end
+
+-- Called each video frame with the time elapsed (Optional)
+-- source_info.video_tick = function(data, seconds)
+    -- set_render_size(data)
+-- end
+
+-- Called when rendering the source with the graphics subsystem
+-- If this is an input/transition source, this is called to draw the source texture with the graphics subsystem.
+-- If this is a filter source, it wraps source draw calls (for example applying a custom effect with custom parameters
+--   to a source). In this case, it’s highly recommended to use the obs_source_process_filter_begin() and
+--   obs_source_process_filter_end() functions to automatically handle effect-based filter processing. However, you
+--   can implement custom draw handling as desired as well.
+-- If the source output capability flags do not include OBS_SOURCE_CUSTOM_DRAW, the source must 
+--   use obs_source_draw() to render the source’s texture.
+-- NOTE: effect – This parameter is no longer used. Instead, call obs_source_draw()
+-- See https://obsproject.com/docs/graphics.html#rendering-video-effect-filters
+source_info.video_render = function(data, effect)
+
+    obslua.obs_source_process_filter_begin(data.source, obslua.GS_RGBA, obslua.OBS_NO_DIRECT_RENDERING)
+
+    -- obslua.gs_effect_set_int(data.params.hue_steps, data.hue_steps)
+    -- obslua.gs_effect_set_int(data.params.value_steps, data.value_steps)
+
+    obslua.obs_source_process_filter_end(data.source, data.effect, data.width, data.height)
+end
+
+-- Called when saving custom data for a source.
+-- This is a separate function because sometimes a source needs to know when it is being saved so it doesn’t always have
+-- to update the current settings until a certain point (Optional)
+source_info.save = function(data, settings)
+    print("In source_info.save")
+end
+
+-- Called when loading custom data from saved source data.
+-- This is called after all the loading sources have actually been created, allowing the ability to reference other
+-- sources if desired (Optional)
+source_info.load = function(data, settings)
+    print("In source_info.load")
+end
+
+-- Called when the filter is removed from a source (Optional)
+source_info.filter_remove = function(data, source)
+    print("In source_info.filter_remove")
+end
+
