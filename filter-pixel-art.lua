925ebe7eed683d115209c68ca2c1359a2b77c614
bfxdev <11587816+bfxdev@users.noreply.github.com>
2020-08-09 10:56:43 +0200

    Added systematic entering/leaving functions logging

--- filter-pixel-art.lua
+++ filter-pixel-art.lua
@@ -24,24 +24,28 @@ LOG_LEVEL_NAMES = {"Debug", "Info", "Warning", "Error", "Nothing"}
 -- Global log level
 log_level = 1
 
--- Recursive transformation of table to string
-function table_to_string(content)
+-- Recursive transformation of any object to string
+function as_string(content)
 
 	local res
 
-	-- Stop condition for recursion
-	if type(content) ~= "table" then
-		res =  tostring(content)
-	
 	-- Lists all elements of a table
-	else
-		res = "{"
-		for k,v in pairs(content) do
-			if string.len(res) > 1 then res = res .. ", " end
-			res = res .. k .. "=" .. table_to_string(v)
-		end
-		res = res .. "}"
-	end
+	if type(content) == "table" then
+        res = "{"
+        for k,v in pairs(content) do
+            if string.len(res) > 1 then res = res .. ", " end
+            res = res .. k .. "=" .. as_string(v)
+        end
+        res = res .. "}"
+
+	-- Lists all elements of a function
+	elseif type(content) == "function" then
+        res = "function" .. as_string(debug.getinfo(content))
+
+    -- Stop condition for recursion
+    else
+        res =  tostring(content)
+    end
 	
 	return res
 end
@@ -49,8 +53,8 @@ end
 -- Main log function, do not use directly
 function log(content, level)
 	if log_level<= level then
-		local str = string.upper(LOG_LEVEL_NAMES[log_level]) .. ": "
-		str = str .. table_to_string(content)
+		local str = string.upper(LOG_LEVEL_NAMES[level]) .. ": "
+		str = str .. as_string(content)
 		print(str)
 	end
 end
@@ -60,6 +64,9 @@ function log_debug(content)   log(content, 1) end
 function log_info(content)    log(content, 2) end
 function log_warning(content) log(content, 3) end
 function log_error(content)   log(content, 4) end
+
+-- log_debug(obslua.obs_data_set_default_int)
+
 
 -------------------------------------------------------- PRESETS -------------------------------------------------------
 
@@ -102,8 +109,7 @@ default_preset_index = 3
 -- Called to retrieve the description string and display it in the script management dialog
 -- NOTE: not documented
 function script_description()
-
-    print("In script_description")
+    log_debug("Entering script_description")
 
     local description = "Pixel Art filter" ..
     "\n\nThis LUA plugin adds a new 'Pixel Art' filter that can be applied on any video source to reduce the " ..
@@ -112,6 +118,7 @@ function script_description()
     "\n\nAs a main global setting, the default preset can be chosen below. " ..
     "Other settings are available when the filter is added on a source."
 
+    log_debug("Leaving script_description\n")
     return description
 
 end
@@ -121,33 +128,40 @@ end
 -- A default value will create the related setting if not already existing, then passed to the first script_load
 -- The parameter settings is of type obs_data_t, see https://obsproject.com/docs/reference-settings.html
 function script_defaults(settings)
+    log_debug("Entering script_defaults")
 
-    print("In script_defaults")
-
-    -- Set default preset at first addition of the script to OBS (if changed later on, then the new value is kept)
+    -- Set default preset and log level at first addition of the script to OBS (if changed later on,
+    -- then the value set by the user will be kept and the set_default commands will have no effect)
     obslua.obs_data_set_default_int(settings, "default_preset_index", default_preset_index)
+    obslua.obs_data_set_default_int(settings, "log_level", log_level)
 
+    log_debug("Leaving script_defaults\n")
 end
 
 -- Called on script startup with specific settings associated with the script.
 function script_load(settings)
+    log_debug("Entering script_load")
 
-    print("In script_load")
-
-    -- Registers the template object source_info
+    -- Registers the template object source_info, used when adding the filter to a video source
+    log_info("Call to obslua.obs_register_source(source_info)")
     obslua.obs_register_source(source_info)
 
+    log_debug("Leaving script_load\n")
 end
 
 -- Called when the script’s settings (if any) have been changed by the user.
 function script_update(settings)
+    log_debug("Entering script_update")
 
-    print("In script_update")
-
+    -- Stores potentially new default main preset as global variable
     default_preset_index = obslua.obs_data_get_int(settings, "default_preset_index")
+    log_info("Current default_preset_index: " .. tostring(obslua.obs_data_get_int(settings, "default_preset_index")))
 
-    print("Current default_preset_index: " .. tostring(obslua.obs_data_get_int(settings, "default_preset_index")))
+    -- Stores potentially new log level as global variable
+    log_level = obslua.obs_data_get_int(settings, "log_level")
+    log_info("Current log_level: " .. tostring(obslua.obs_data_get_int(settings, "log_level")))
 
+    log_debug("Leaving script_update\n")
 end
 
 -- Called to define user properties associated with the script.
@@ -155,31 +169,40 @@ end
 -- Returns: obs_properties_t object created via obs_properties_create().
 function script_properties()
 
-    print("In script_properties")
+    log_debug("Entering script_properties")
 
-    -- Creates properties object to be displayed on dialog window
+    -- Creates preset properties object to be displayed on dialog window
     local properties = obslua.obs_properties_create()
+
+    -- List of main presets
     local list = obslua.obs_properties_add_list(properties, "default_preset_index", "Default Preset",
                                                 obslua.OBS_COMBO_TYPE_LIST, obslua.OBS_COMBO_FORMAT_INT)
-
     -- Fills list with values from presets except PRESET_CUSTOM (special value only displayed once some
     -- other setting part of the preset was changed)
     for k,v in ipairs(presets) do
         if k ~= PRESET_CUSTOM then obslua.obs_property_list_add_int(list, v.name, k) end
     end
-  
+
+    -- List of log levels
+    list = obslua.obs_properties_add_list(properties, "log_level", "Log Level",
+                                          obslua.OBS_COMBO_TYPE_LIST, obslua.OBS_COMBO_FORMAT_INT)
+    for k,v in ipairs(LOG_LEVEL_NAMES) do obslua.obs_property_list_add_int(list, v, k) end
+
+    log_debug("Leaving script_properties\n")
     return properties
 end
 
 -- Called when the script is being saved. This is not necessary for settings that are set by the user,
 -- instead this is used for any extra internal settings data that may be used in the script.
 function script_save(settings)
-    print("In script_save")
+    log_debug("Entering script_save")
+    log_debug("Leaving script_save\n")
 end
 
 -- Called when the script is being unloaded.
 function script_unload()
-    print("In script_unload\n")
+    log_debug("Entering script_unload")
+    log_debug("Leaving script_unload\n")
 end
 
 ------------------------------------------------------ EFFECT FILES ----------------------------------------------------
@@ -297,8 +320,7 @@ end
 
 -- Sets the default settings for this source
 source_info.get_defaults = function(settings)
-
-    print("In source_info.get_defaults")
+    log_debug("Entering source_info.get_defaults")
 
     -- Main preset (possibly set to PRESET_CUSTOM)
     obslua.obs_data_set_default_int(settings, "preset_index", default_preset_index)
@@ -316,12 +338,12 @@ source_info.get_defaults = function(settings)
 		obslua.obs_data_set_default_int(settings, k, v)
 	end
 
+    log_debug("Leaving source_info.get_defaults\n")
 end
 
 -- Updates the settings for this source (Optional)
 source_info.update = function(data, settings)
-
-    log_debug("In source_info.update")
+    log_debug("Entering source_info.update")
 	
 	-- Makes sure that palette_type reflects the palette selected by the user
 	local palette_index = obslua.obs_data_get_int(settings, "palette_index")
@@ -356,15 +378,17 @@ source_info.update = function(data, settings)
         obslua.obs_leave_graphics()
     end ]]--
 
+    log_debug("Leaving source_info.update\n")
 end
 
 
 function property_modified(props, property, settings)
-
-	print("In property_modified for " .. obslua.obs_property_name(property) .. " forced_change=" .. tostring(obslua.obs_data_get_bool(settings, "forced_change")))
+    log_debug("Entering property_modified for " .. obslua.obs_property_name(property) ..
+              " forced_change=" .. tostring(obslua.obs_data_get_bool(settings, "forced_change")))
 	
     -- Checks and sets flag to prevent cascaded changes
     if obslua.obs_data_get_bool(settings, "forced_change") then
+        log_debug("Leaving property_modified without action for " .. obslua.obs_property_name(property))
         return false
     end
     obslua.obs_data_set_bool(settings, "forced_change", true)
@@ -415,15 +439,15 @@ function property_modified(props, property, settings)
 
     -- Resets flag and returns true if a refersh is necessary
     obslua.obs_data_set_bool(settings, "forced_change", false)
+
+    log_debug("Leaving property_modified for " .. obslua.obs_property_name(property))
     return refresh
 end
-
-
 
 -- Sets visible flags of the displayed properties to hide unnecessary parameters
 -- Callback on several properties (called as well at first properties widget display)
 function set_properties_visibility(props, property, settings)
-    print("In set_properties_visibility callback")
+    -- print("In set_properties_visibility callback")
 
     -- Retrieves values from the settings
     local palette_index = obslua.obs_data_get_int(settings, "palette_index")
@@ -448,8 +472,7 @@ end
 
 -- Gets the property information of this source (Optional)
 source_info.get_properties = function(data)
-
-    print("In source_info.get_properties")
+    log_debug("Entering source_info.get_properties")
 
     -- Always re-create object, and keeps a ref on it
     data.props = obslua.obs_properties_create()
@@ -493,6 +516,7 @@ source_info.get_properties = function(data)
 	-- Activates changes based on property callback
     obslua.obs_data_set_bool(data.settings, "forced_change", false)
 
+    log_debug("Leaving source_info.get_properties\n")
     return data.props
 end
 
@@ -501,8 +525,7 @@ end
 --              source – Source that this data is associated with
 -- Returns:	The implementation data associated with this source
 source_info.create = function(settings, source)
-
-    print("In source_info.create")
+    log_debug("Entering source_info.create")
 
     -- Inits filter data object (arbitrary data structure used to keep own data between calls)
     data = {}
@@ -530,22 +553,24 @@ source_info.create = function(settings, source)
         return nil
     end
 
-    -- Calls update to initialize the rest of the propertis-managed settings
+    -- Calls update to initialize the rest of the properties-managed settings
     source_info.update(data, settings)
 
+    log_debug("Leaving source_info.create\n")
     return data
 end
 
 -- Destroys the implementation data for the source
 source_info.destroy = function(data)
-
-    print("In source_info.destroy")
+    log_debug("Entering source_info.destroy")
 
     if data.effect ~= nil then
         obslua.obs_enter_graphics()
         obslua.gs_effect_destroy(data.effect)
         obslua.obs_leave_graphics()
     end
+
+    log_debug("Leaving source_info.destroy\n")
 end
 
 -- Returns the width of the source, this callback is required if this is a video source and is synchronous
@@ -574,22 +599,26 @@ end
 
 -- Called when the source has been activated in the main view (visible on stream/recording) (Optional)
 source_info.activate = function(data)
-    print("In source_info.activate")
+    log_debug("Entering source_info.activate")
+    log_debug("Leaving source_info.activate\n")
 end
 
 -- Called when the source has been deactivated in the main view (no longer visible on stream/recording) (Optional)
 source_info.deactivate = function(data)
-    print("In source_info.deactivate")
+    log_debug("Entering source_info.deactivate")
+    log_debug("Leaving source_info.deactivate\n")
 end
 
 -- Called when the source is visible on any display and/or on the main view (Optional) 
 source_info.show = function(data)
-    print("In source_info.show")
+    log_debug("Entering source_info.show")
+    log_debug("Leaving source_info.show\n")
 end
 
 -- Called when the source is no longer visible on any display and/or on the main view (Optional) 
 source_info.hide = function(data)
-    print("In source_info.hide")
+    log_debug("Entering source_info.hide")
+    log_debug("Leaving source_info.hide\n")
 end
 
 -- Called each video frame with the time elapsed (Optional)
@@ -655,3 +684,6 @@ for key,value in ipairs(presets) do
 end
 print("") ]]
 
+log_debug("Definition of the palettes:")
+log_debug(palettes)
+-- log_debug(obslua)    to be debugged
