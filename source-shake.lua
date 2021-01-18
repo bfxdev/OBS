97f526d585417c910b9d80773f5d3179c6a17986
bfxdev <11587816+bfxdev@users.noreply.github.com>
2021-01-18 08:31:41 +0100

    Adaptation of links for Wiki

--- source-shake.lua
+++ source-shake.lua
@@ -75,7 +75,7 @@ function script_description()
   return [[<center><h2>Source Shake!!</h2></center>
            <p>Shake a source in the current scene when a hotkey is pressed. Go to <em>Settings
            </em> then <em>Hotkeys</em> to select the key combination.</p><p>Check the <a href=
-           "https://github.com/obsproject/obs-studio/wiki/Scripting-Tutorial-Source-Shake.md">
+           "https://github.com/obsproject/obs-studio/wiki/Scripting-Tutorial-Source-Shake">
            Source Shake Scripting Tutorial</a> on the OBS Wiki for more information.</p>]]
 end
 
--- source-shake.py
+++ source-shake.py
@@ -6,7 +6,7 @@ def script_description():
   return """<center><h2>Source Shake!!</h2></center>
             <p>Shake a source in the current scene when a hotkey is pressed. Go to <em>Settings
             </em> then <em>Hotkeys</em> to select the key combination.</p><p>Check the <a href=
-            "https://github.com/obsproject/obs-studio/wiki/Scripting-Tutorial-Source-Shake.md">
+            "https://github.com/obsproject/obs-studio/wiki/Scripting-Tutorial-Source-Shake">
             Source Shake Scripting Tutorial</a> on the OBS Wiki for more information.</p>"""
 
 # Global variables to restore the scene item after shake
